87b364e31667c5d1c776638b034cb164ba742376
Pratik Singh Rajput <spratik887@gmail.com>
2026-07-23 10:40:44 +0530

    login page styling error fixed

--- Frontend/app/login/page.tsx
+++ Frontend/app/login/page.tsx
@@ -15,6 +15,7 @@ export default function LoginPage() {
   const { setUser } = useAuth();
 
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [loading, setLoading] = useState(false);
 
